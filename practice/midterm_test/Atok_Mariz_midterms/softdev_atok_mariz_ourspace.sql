3efea9490b83268b724ee31daab6b9df522171d7
Mariz <marizatok26@gmail.com>
2015-03-01 15:01:35 +0800

    added 1 record for myaddress and mycomment

--- practice/midterm_test/Atok_Mariz_midterms/softdev_atok_mariz_ourspace.sql
+++ practice/midterm_test/Atok_Mariz_midterms/softdev_atok_mariz_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2015 at 07:36 AM
+-- Generation Time: Mar 01, 2015 at 07:58 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -38,6 +38,13 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `cellphone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1234, 'mariz', 'bautista', 'atok', 'f', '2015-03-01 06:55:18', 'parañaque city', '8888888', '09123456789');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +58,13 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `body` longtext NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(4321, 1234, 'yuki', 'test', '2015-02-28 16:00:00');
 
 --
 -- Indexes for dumped tables
