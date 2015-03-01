68b089ceab52001b08161d6c96c606650170488c
Mariz <marizatok26@gmail.com>
2015-03-01 15:42:04 +0800

    updated filed myaddress_id in the form mycomment to show last name as a dropdown list

--- practice/midterm_test/Atok_Mariz_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Atok_Mariz_midterms/ourspace/views/mycomment/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Myaddress;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\mycomment */
@@ -13,14 +15,30 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'id')->textInput() ?>
+	
+  
+        <?php
+        $myaddress=myaddress::find()->all();
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+        $listData=ArrayHelper::map($myaddress,'id','lastname');
+
+ 
+        echo $form->field($model, 'myaddress_id')->dropDownList(
+                                $listData,
+
+                                ['prompt'=>'Select Lastname...']);
+
+		?>
+
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
+	
+
+	
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
