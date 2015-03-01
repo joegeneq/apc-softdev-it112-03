adc6092b3c8dee0e0c4c6ec5aa51ee81a46a5e14
Ryan Ric Alegre <ryanric.alegre@gmail.com>
2015-03-02 01:01:46 +0800

    Updated field myaddress_id in the form for mycomment to show the lastname as a dropdown list

--- practice/midterm_test/Alegre_Ryan_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Alegre_Ryan_midterms/ourspace/views/mycomment/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Myaddress;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
@@ -14,7 +16,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+	<?= $form->field($model, 'myaddress_id')->dropDownList(
+		ArrayHelper::map(Myaddress::find()->all(), 'id', 'lastname'),
+		['prompt'=>'Select lastname']
+	)?>
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
