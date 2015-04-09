93aff8becb17c8748d1375fc9363cad66b5ab5f7
Julie Anne Cortez <cortezleean@gmail.com>
2015-04-09 22:47:21 +0800

    Modified backend/views/event-list/_form.php Event List fields

--- application/philhealthngmasa-adv/backend/views/event-list/_form.php
+++ application/philhealthngmasa-adv/backend/views/event-list/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'el_ctrlno')->textInput() ?>
-
     <?= $form->field($model, 'el_date')->textInput() ?>
 
     <?= $form->field($model, 'el_event')->textInput(['maxlength' => 45]) ?>
