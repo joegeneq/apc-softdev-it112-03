17908bfe66233a13e0198897f46a3c0d8b02f789
Julie Anne Cortez <cortezleean@gmail.com>
2015-04-09 23:58:41 +0800

    Modified backend/views/event-list/_form.php dropdown

--- application/philhealthngmasa-adv/backend/views/event-list/_form.php
+++ application/philhealthngmasa-adv/backend/views/event-list/_form.php
@@ -16,9 +16,9 @@ use dosamigos\datepicker\DatePicker;
     <?= $form->field($model, 'el_date')->widget(
          DatePicker::className(), [
         // inline too, not bad
-         'inline' => true, 
+         'inline' => false, 
          // modify template for custom rendering
-        'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
         'clientOptions' => [
             'autoclose' => true,
             'format' => 'yyyy-mm-dd'
